b68a7cbdca03f0dd540c50c2efa90b958d00cb29
JoseGaitanM <jgaitan@griddynamics.com>
2023-04-12 00:55:19 -0600

    Add container for testing

--- dags/spark_jobs/create_snapshoots_job.py
+++ dags/spark_jobs/create_snapshoots_job.py
@@ -1,26 +1,16 @@
 from pyspark.sql import SparkSession
 from datetime import date, timedelta
-from pyspark.sql.types import StructType, StructField, StringType, LongType, MapType
-from pyspark.sql.functions import max, min, col, lit, row_number
+from pyspark.sql.functions import max, col, lit, row_number
 import os
 from pyspark.sql.window import Window
-from pyspark.sql.functions import input_file_name, regexp_extract
-
-schemaSubscriptions = StructType([
-  StructField("subscription", StringType(), False),
-  StructField("numberOfChannels", LongType(), False),
-  StructField("extras", MapType(StringType(), StringType(), valueContainsNull = True), nullable = True)
-])
 
 spark = SparkSession.builder.master("local[1]") \
     .appName("Create snapshot") \
     .getOrCreate()
 spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
 
-##Case snapshots exists
-if os.listdir("/opt/airflow/data/files/snapshots"):
-
-  dfsnapshots = spark.read.parquet("/opt/airflow/data/files/snapshots")
+def getMaxDateSnapshots(path):
+  dfsnapshots = spark.read.parquet(path)
   dfsnapshots.show(n=1000, truncate=False)
 
   datessnapshots = dfsnapshots.select("date").distinct()
@@ -29,47 +19,93 @@ if os.listdir("/opt/airflow/data/files/snapshots"):
   maxDatesnapshots = datessnapshots.agg(max('date')).collect()[0][0]
   print('Max date snapshots: ',maxDatesnapshots)
 
-  snapshoot = spark.read.parquet(f'/opt/airflow/data/files/snapshots/date={maxDatesnapshots.strftime("%Y-%m-%d")}')
+  return maxDatesnapshots
+
+def readMaxDateSnapshoots(path,maxDatesnapshots):
+  snapshoot = spark.read.parquet(f'{path}/date={maxDatesnapshots.strftime("%Y-%m-%d")}')
   snapshoot.show(n=1000, truncate=False)
   snapshoot.drop('date')
 
+  return snapshoot
+
+def getSubscriptions(path):
+  subscriptions = spark.read.parquet(path)
+  subscriptions.show(n=1000, truncate=False)
+
+  return subscriptions
+
+
+def getMaxDateRegisters(path):
+  registers = spark.read.option("multiline","true").json(path)
+  registers.show(n=1000, truncate=False)
+
+  distinct_dates = registers.select("date").distinct()
+  distinct_dates.show()
+
+  dateData = distinct_dates.agg(max('date')).collect()[0][0]
+  print('Date data json: ', dateData)
+
+  return (dateData, registers)
+
+def enrichNewRegisters(subscriptions):
+  registers = spark.read.option("multiline","true").json(f'/opt/airflow/data/files/registers')
+  registers[registers['date'] > maxDatesnapshots]
+  registers.show(n=1000, truncate=False)
+
+  joined = registers.join(subscriptions, ['subscription'])
+  joined.show(n=1000, truncate=False)
+
+  joined.printSchema()
+  snapshoot.printSchema()
+
+  return joined
+
+
+def createNewSnapshot(joined,dateData):
+  latestData = (joined
+                .withColumn("rowNumber", row_number().over(Window.partitionBy(col("id")).orderBy(col("date").desc())))
+                .where(col("rowNumber") == lit(1))
+                .drop("rowNumber"))
+  
+  latestData.show(n=1000, truncate=False)
+  latestData.write.mode("overwrite").parquet(f'/opt/airflow/data/files/snapshots/date={dateData.strftime("%Y-%m-%d")}')
+
+  return latestData
+
+def enrichData(registers,subscription):
+
+  joined = registers.join(subscription, ['subscription'])
+  joined.show(n=1000, truncate=False)
+
+  return joined
+
+snapshotsPath = "/opt/airflow/data/files/snapshots"
+subscriptionsPath = "/opt/airflow/data/files/subscriptions"
+registersPath = "/opt/airflow/data/files/registers"
+
+
+if os.listdir("/opt/airflow/data/files/snapshots"):
+
+  ###get max date snapshots
+  maxDatesnapshots = getMaxDateSnapshots(snapshotsPath)
+
+  ###read max snapshots
+  snapshoot = readMaxDateSnapshoots(snapshotsPath,maxDatesnapshots)
+
   if os.listdir("/opt/airflow/data/files/registers"):
 
-    df2 = spark.read.parquet("/opt/airflow/data/files/subscriptions")
-    df2.show(n=1000, truncate=False)
+    #####get subscriptions
+    subscriptions = getSubscriptions(subscriptionsPath)
 
-    df1 = spark.read.option("multiline","true").json("/opt/airflow/data/files/registers")
-    df1.show(n=1000, truncate=False)
-
-    distinct_dates = df1.select("date").distinct()
-    distinct_dates.show()
-
-    dateData = distinct_dates.agg(max('date')).collect()[0][0]
-    print('Date data json: ', dateData)
+    ###get max date registers
+    dateData,_ = getMaxDateRegisters(registersPath)
 
     if (dateData > maxDatesnapshots):
+      ###enrich new registers
+      joined = enrichNewRegisters(subscriptions)
 
-      data = spark.read.option("multiline","true").json(f'/opt/airflow/data/files/registers')
-      data[data['date'] > maxDatesnapshots]
-      data.show(n=1000, truncate=False)
-
-      joined = data.join(df2, ['subscription'])
-      joined.show(n=1000, truncate=False)
-
-      joined.printSchema()
-      snapshoot.printSchema()
-
-      union = joined.union(snapshoot)
-      union.show(n=1000, truncate=False)
-
-      latestData = (joined
-                     .withColumn("rowNumber", row_number().over(Window.partitionBy(col("id")).orderBy(col("date").desc())))
-                     .where(col("rowNumber") == lit(1))
-                     .drop("rowNumber"))
-      
-      latestData.show(n=1000, truncate=False)
-      latestData.write.mode("overwrite").parquet(f'/opt/airflow/data/files/snapshots/date={dateData.strftime("%Y-%m-%d")}')
-
+      ###create new snapshoot
+      latestData = createNewSnapshot(joined,dateData)
     else:
       print('snapshots Up to date')
 
@@ -79,26 +115,15 @@ if os.listdir("/opt/airflow/data/files/snapshots"):
 else:
    
    if os.listdir("/opt/airflow/data/files/registers"):
-    
-    df2 = spark.read.parquet("/opt/airflow/data/files/subscriptions")
-    df2.show(n=1000, truncate=False)
 
-    df1 = spark.read.option("multiline","true").json("/opt/airflow/data/files/registers")
-    df1.show(n=1000, truncate=False)
+    #####get subscriptions
+    subscriptions = getSubscriptions(subscriptionsPath)
 
-    distinct_dates = df1.select("date").distinct()
-    distinct_dates.show()
+    ###get max date registers
+    dateData,registers = getMaxDateRegisters(registersPath)
 
-    dateData = distinct_dates.agg(max('date')).collect()[0][0]
-    print('Date data json: ', dateData)
+    ##enrich data
+    joined = enrichData(registers,subscriptions)
 
-    joined = df1.join(df2, ['subscription'])
-    joined.show(n=1000, truncate=False)
-
-    latestData = (joined
-                     .withColumn("rowNumber", row_number().over(Window.partitionBy(col("id")).orderBy(col("date").desc())))
-                     .where(col("rowNumber") == lit(1))
-                     .drop("rowNumber"))
-    
-    latestData.show(n=1000, truncate=False)
-    latestData.write.mode("overwrite").parquet(f'/opt/airflow/data/files/snapshots/date={dateData.strftime("%Y-%m-%d")}/')
+    ###create new snapshoot
+    latestData = createNewSnapshot(joined,dateData)
